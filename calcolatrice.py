48e86f16e1eb764d94f94b919ec595e4a30542bd
ElghattasYoussef <elghattas.youssefwalidyo.08@itisdalmine.edu.it>
2024-12-17 09:57:17 +0100

    Update calcolatrice.py

--- calcolatrice.py
+++ calcolatrice.py
@@ -8,16 +8,3 @@ def sottrazione():
     n_2 = int(input("Inserisci il secondo numero: "))
     risultato = n_1 - n_2
     print(f"{n_1} - {n_2} = {risultato}")
- 
-while True:
-    print("Benvenuto nella nostra calcolatrice")
-    print("Inserisci l'operazione che vuoi effettuare")
-    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
-    if scelta == 0:
-        break
-    elif scelta == 1:
-        sottrazione() 
-    elif scelta == 2:
-        Somma()
-    else:
-        print("Scelta non corretta!")
