b49a1700210e7ad73c3a82ac5d3daa742fabe300
ElghattasYoussef <elghattas.youssefwalidyo.08@itisdalmine.edu.it>
2024-12-17 09:44:13 +0100

    Update calcolatrice.py
    def

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,19 +1,21 @@
-scelta=-1
-while True: 
+def Somma():
+    n_1 = int(input("Inserisci il primo numero: "))
+    n_2 = int(input("Inserisci il secondo numero: "))
+    risultato = n_1 + n_2
+    print(f"{n_1} + {n_2} = {risultato}")
+ 
+while True:
     print("Benvenuto nella nostra calcolatrice")
     print("Inserisci l'operazione che vuoi effettuare")
-    scelta = int(input("1)sotazione\n2)addizione\n0)exit"))
+    scelta = int(input("1)sottrazione\n2)addizione\n0)exit\n"))
     if scelta == 0:
         break
     elif scelta == 1:
-        n_1=int(input("Inserisci il primo numero: "))
-        n_2=int(input("Inserisci il secondo numero: "))
+        n_1 = int(input("Inserisci il primo numero: "))
+        n_2 = int(input("Inserisci il secondo numero: "))
         risultato = n_1 - n_2
         print(f"{n_1} - {n_2} = {risultato}")
     elif scelta == 2:
-        n_1=int(input("Inserisci il primo numero: "))
-        n_2=int(input("Inserisci il secondo numero: "))
-        risultato = n_1 + n_2
-        print(f"{n_1} + {n_2} = {risultato}")
+        Somma()
     else:
-        print("Scelta non coretta!")
+        print("Scelta non corretta!")
